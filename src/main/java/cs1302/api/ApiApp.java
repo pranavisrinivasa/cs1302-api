de0a2e93973730d1016c6a96b2d78ed4517df3d1
Pranavi Srinivasa <ps14999@csci-odin.cs.uga.edu>
2023-04-29 13:00:43 -0400

    commit1test

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -1,11 +1,20 @@
 package cs1302.api;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -13,16 +22,33 @@ import javafx.stage.Stage;
  * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
  */
 public class ApiApp extends Application {
+/*    public static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)
+        .folloRedirects(HttpClient.Redirect.NORMAL)
+        .build();
+
+    public static Gson GSON = new GsonBuilder()
+        .setprettyPrinting()
+        .create();
+*/
     Stage stage;
     Scene scene;
     VBox root;
+    HBox hbox;
+    VBox left;
+    VBox right;
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
      * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
      */
+
     public ApiApp() {
         root = new VBox();
+        hbox = new HBox();
+        left = new VBox();
+        right = new VBox();
+
     } // ApiApp
 
     /** {@inheritDoc} */
@@ -32,16 +58,33 @@ public class ApiApp extends Application {
         this.stage = stage;
 
         // demonstrate how to load local asset using "file:resources/"
-        Image bannerImage = new Image("file:resources/readme-banner.png");
+        Image bannerImage = new Image("file:resources/header.png");
+        Image boxLeft = new Image("file:resources/box.png");
+        Image boxRight = new Image("file:resources/box.png");
+
         ImageView banner = new ImageView(bannerImage);
+        ImageView imgLeft = new ImageView(boxLeft);
+        ImageView imgRight = new ImageView(boxRight);
         banner.setPreserveRatio(true);
         banner.setFitWidth(640);
+        imgLeft.setPreserveRatio(true);
+        imgLeft.setFitWidth(320);
+        imgRight.setPreserveRatio(true);
+        imgRight.setFitWidth(320);
 
+        // buttons
+        Button generate = new Button("Generate recipe");
+        generate.setLayoutX(100);
+        generate.setLayoutY(10);
         // some labels to display information
         Label notice = new Label("Modify the starter code to suit your needs.");
 
         // setup scene
-        root.getChildren().addAll(banner, notice);
+        //       left.getChildren().addAll(generate, imgLeft);
+        right.getChildren().addAll(imgRight);
+        StackPane sp = new StackPane(imgLeft, generate);
+        hbox.getChildren().addAll(sp, right);
+        root.getChildren().addAll(banner, hbox, notice);
         scene = new Scene(root);
 
         // setup stage
